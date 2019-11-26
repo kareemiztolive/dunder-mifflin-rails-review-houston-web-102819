d401ec861b7d6cce478a9335d2ec2249647cf340
kareemiztolive <nelsonsmf@gmail.com>
2019-11-26 16:44:26 -0600

    dunder-mifflin-project

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,13 @@
 class DogsController < ApplicationController
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+
+    def index
+        @dogs = Dog.all
+    end
+
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,59 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+    
+    def new
+        @dogs = Dog.all
+    end
+
+    def create
+        Employee.all.each do |employee|
+            if employee.alias == params[:alias]
+                params[:alias] = "none"
+            end
+            if employee.title == params[:title]
+                params[:title] = "none"
+            end
+        end
+        
+        Employee.create({
+           first_name: params[:first_name],
+           last_name: params[:last_name],
+           alias: params[:alias],
+           title: params[:title],
+           office: params[:office],
+           img_url: params[:img_url],
+           dog_id: params[:dog]
+        })
+        redirect_to '/employees'
+    end
+    
+    def edit
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+    end
+
+    def update
+        employee = Employee.find(params[:id])
+        employee.update({
+            first_name: params[:first_name],
+            last_name: params[:last_name],
+            alias: params[:alias],
+            title: params[:title],
+            office: params[:office],
+            img_url: params[:img_url],
+            dog_id: params[:dog]
+         })
+         redirect_to '/employees'
+    end
+
+ 
+
+
 end
